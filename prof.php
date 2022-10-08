428229068f76c7eebeee66607e28f6cde2a983b1
Nijoy007 <110754818+Nijoy007@users.noreply.github.com>
2022-10-08 16:28:26 +0600

    Add files via upload

---
+++ prof.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>new profile uijj</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Supherhero">
+    <meta name="keywords" content="programming hero,learn programming">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/vnd.microsoft.icon">
+    
+
+    <!--custom css-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav>
+
+<img src="profile.png" class="png"> <span class="text" ><?php $txt = $_REQUEST['text']; echo $txt;?> 
+
+
+    <h3>Hello World</h3>
+    <form action ="index.html" method="POST">
+    <input type="num" class="view" placeholder="post something" name="text">
+    <input type="submit" value="post">
+
+    
+
+
+<!--main js-->
+<script src="js/main.js"></script>
+</body>
+</html>
+<style>
+.acc{
+margin-top:60px;
+margin-left:50px;
+font-size:28px;
+color: black;
+}
+body{
+ font-family: "Lato", sans-serif;
+ background-color:#FFF5EE;
+}
+.bal{
+margin-top:35px;
+
+
+
+}
+.h3{
+color:#1473ef;
+font-size:18px;
+margin-left:40px;
+color:#47e6f6;
+margin-top:30px;
+}
+.h4{
+
+margin-left:40px;
+color:#FAC898;
+font-size:25px;
+}
+.text{
+margin-top:36px;
+margin-left:200px;
+color:#1473ef;
+display: inline-block;
+background-color:#fff;
+border-radius:12px;
+padding:10px;
+}
+.png{
+width:47px;
+height:47px;
+margin-left:22px;
+margin-top:30px;
+display: inline-block;
+float:left;
+margin-bottom:20px;
+}
+.pngs{
+width:47px;
+height:47px;
+margin-left:150px;
+margin-top:0px;
+display: inline-block;
+float:left;
+margin-bottom:5px;
+}
+.pngl{
+width:40px;
+height:40px;
+margin-left:150px;
+margin-top:0px;
+display: inline-block;
+float:left;
+margin-bottom:5px;
+}
+
+
+nav{
+display: inline-block;
+font-family: system-ui;
+  background-image: url("images (28).jpeg");
+  position: relative;
+  height:200px;
+  margin-left:-10px;
+  margin-right:-10px;
+  margin-top:-10px;
+  width:450px;
+  background-size:cover;
+  }
+  
+  
+  
+  .boxx{
+background-color:#fff;
+width:350px;
+height:150px;
+padding:20px;
+margin-left:5px;
+border-radius:10px;
+color:black;
+margin-top:60px;
+outline:none;
+margin-bottom:40px;
+}
+a{
+text-decoration:none;
+}
+
+
+
+@media screen and (min-width: 480px) {
+
+.boxx{
+background-color:#fff;
+width:500px;
+height:150px;
+padding-left:200px;
+margin-left:40px;
+border-radius:10px;
+color:black;
+margin-top:60px;
+outline:none;
+margin-bottom:40px;
+}
+}
+
+
+
+</style>
